af16192fb87b5757c02ef0141e9dd5d499125fb2
Kyle Springfield <kspringfield13@gmail.com>
2023-11-20 08:21:52 -0500

    added bitcoin and eth tickers

--- app.py
+++ app.py
@@ -52,50 +52,71 @@ def get_sp500_ticker():
 
     return sp500_values
 
-def get_nasdaq_ticker():
-    # Fetch data for the NASDAQ
-    nasdaq = yf.Ticker("^IXIC")
-    nasdaq_data = nasdaq.history(period="1d")
+def get_bitcoin_data(api_key):
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': api_key,
+    }
+    parameters = {
+        'start': '1',
+        'limit': '1',
+        'convert': 'USD'
+    }
+    
+    response = requests.get(url, headers=headers, params=parameters)
+    data = response.json()
 
-    # Get the latest data point
-    latest_data = nasdaq_data.iloc[-1]
-    price = latest_data['Close']
-    change = price - latest_data['Open']
-    change_percent = (change / latest_data['Open']) * 100
-
+    # Assuming Bitcoin is the first in the list
+    bitcoin_data = data['data'][0]
+    price = float(bitcoin_data['quote']['USD']['price'])
+    change_percent = float(bitcoin_data['quote']['USD']['percent_change_24h'])
+    
     # Determine the sign of the change
     change_sign = 0
-    if change_percent < 0:
-        change_sign = -1
-    elif change_percent > 0:
+    if change_percent > 0:
         change_sign = 1
+    elif change_percent < 0:
+        change_sign = -1
+    
+    return ['Bitcoin', float(price), float(change_percent), change_sign]
 
-    nasdaq_values = ['NASDAQ', float(price), float(change_percent), change_sign]
+def get_ethereum_data(api_key):
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': api_key,
+    }
+    parameters = {
+        'start': '1',
+        'limit': '2',
+        'convert': 'USD'
+    }
+    
+    response = requests.get(url, headers=headers, params=parameters)
+    data = response.json()
 
-    return nasdaq_values
-
-def get_dow_ticker():
-    # Fetch data for the Dow Jones
-    dow = yf.Ticker("^DJI")
-    dow_data = dow.history(period="1d")
-
-    # Get the latest data point
-    latest_data = dow_data.iloc[-1]
-    price = latest_data['Close']
-    change = price - latest_data['Open']
-    change_percent = (change / latest_data['Open']) * 100
-
+    # Assuming Bitcoin is the first in the list
+    bitcoin_data = data['data'][1]
+    price = float(bitcoin_data['quote']['USD']['price'])
+    change_percent = float(bitcoin_data['quote']['USD']['percent_change_24h'])
+    
     # Determine the sign of the change
     change_sign = 0
-    if change_percent < 0:
-        change_sign = -1
-    elif change_percent > 0:
+    if change_percent > 0:
         change_sign = 1
+    elif change_percent < 0:
+        change_sign = -1
+    
+    return ['Ethereum', float(price), float(change_percent), change_sign]
 
-    dow_values = ['DJIA', float(price), float(change_percent), change_sign]
+key = 'cd2698aa-18bc-4c59-bb6d-73bfdc540887'
 
-    return dow_values
-
+def render_svg(svg):
+    """Renders the given svg string."""
+    b64 = base64.b64encode(svg.encode('utf-8')).decode("utf-8")
+    html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
+    st.write(html, unsafe_allow_html=True)
 
 st.markdown(
     """
@@ -130,11 +151,6 @@ with open("images/logo.png", "rb") as f:
     data = f.read()
     encoded = base64.b64encode(data)
 logo = "data:image/png;base64," + encoded.decode("utf-8")
-
-with open("images/banner.png", "rb") as f:
-    data = f.read()
-    encoded = base64.b64encode(data)
-banner = "data:image/png;base64," + encoded.decode("utf-8")
 
 cur_url = st_javascript("await fetch('').then(r => window.parent.location.href)")
 
@@ -194,17 +210,17 @@ with s_column:
 
         tlc, trc = st.columns((5,5),gap="small")
         with tlc:
-            st.markdown(f'<div style="text-align: left; color: white; font-weight: bold; font-size: 16px;">{name}</div>', unsafe_allow_html=True)
+            render_svg("""<svg xmlns="http://www.w3.org/2000/svg" height="32" width="32"><path d="M16 32C7.163 32 0 24.837 0 16S7.163 0 16 0s16 7.163 16 16-7.163 16-16 16zm6.5-12.846c0-2.523-1.576-3.948-5.263-4.836v-4.44c1.14.234 2.231.725 3.298 1.496l1.359-2.196a9.49 9.49 0 00-4.56-1.776V6h-2.11v1.355c-3.032.234-5.093 1.963-5.093 4.486 0 2.64 1.649 3.925 5.19 4.813v4.58c-1.577-.234-2.886-.935-4.269-2.01L9.5 21.35a11.495 11.495 0 005.724 2.314V26h2.11v-2.313c3.08-.257 5.166-1.963 5.166-4.533zm-7.18-5.327c-1.867-.537-2.327-1.168-2.327-2.15 0-1.027.8-1.845 2.328-1.962zm4.318 5.49c0 1.122-.873 1.893-2.401 2.01v-4.229c1.892.538 2.401 1.168 2.401 2.22z" fill="#fff" fill-rule="evenodd"/></svg>""")
             
         with trc:
-            st.markdown(f'<div style="text-align: right; color: white; font-weight: bold; font-size: 16px;">{price:,.2f}</div>', unsafe_allow_html=True)
+            st.markdown(f'<div style="text-align: right; color: white; font-weight: bold; font-size: 16px;">${price:,.2f}</div>', unsafe_allow_html=True)
 
         tlc_2, trc_2 = st.columns((5,5),gap="small")
         with tlc_2:
-            st.markdown(f'<div style="text-align: left; color: {color}; font-weight: bold; font-size: 16px;">{arrow}</div>', unsafe_allow_html=True)
+            st.markdown(f'<div style="text-align: left; color: white; font-weight: bold; font-size: 16px;">{name}</div>', unsafe_allow_html=True)
             
         with trc_2:
-            st.markdown(f'<div style="text-align: right; color: {color}; font-weight: bold; font-size: 16px;">{abs(change_percent):.2f}%</div>', unsafe_allow_html=True)
+            st.markdown(f'<div style="text-align: right; color: {color}; font-weight: bold; font-size: 16px;">{abs(change_percent):.2f}%  {arrow}</div>', unsafe_allow_html=True)
 
     st.markdown(
     """
@@ -221,9 +237,9 @@ with s_column:
 
 with t_column:
     
-    # NASDAQ
-    nasdaq_values = get_nasdaq_ticker()
-    name, price, change_percent, sign = nasdaq_values
+    # Bitcoin
+    bitcoin_values = get_bitcoin_data(key)
+    name, price, change_percent, sign = bitcoin_values
 
     # Define the arrow symbol based on the sign of the change
     arrow = "▲" if sign == 1 else "▼" if sign == -1 else ""
@@ -235,17 +251,17 @@ with t_column:
 
         tlc, trc = st.columns((5,5),gap="small")
         with tlc:
-            st.markdown(f'<div style="text-align: left; color: white; font-weight: bold; font-size: 16px;">{name}</div>', unsafe_allow_html=True)
-            
+            render_svg("""<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"><g fill="none" fill-rule="evenodd"><circle cx="16" cy="16" r="16" fill="#F7931A"/><path fill="#FFF" fill-rule="nonzero" d="M23.189 14.02c.314-2.096-1.283-3.223-3.465-3.975l.708-2.84-1.728-.43-.69 2.765c-.454-.114-.92-.22-1.385-.326l.695-2.783L15.596 6l-.708 2.839c-.376-.086-.746-.17-1.104-.26l.002-.009-2.384-.595-.46 1.846s1.283.294 1.256.312c.7.175.826.638.805 1.006l-.806 3.235c.048.012.11.03.18.057l-.183-.045-1.13 4.532c-.086.212-.303.531-.793.41.018.025-1.256-.313-1.256-.313l-.858 1.978 2.25.561c.418.105.828.215 1.231.318l-.715 2.872 1.727.43.708-2.84c.472.127.93.245 1.378.357l-.706 2.828 1.728.43.715-2.866c2.948.558 5.164.333 6.097-2.333.752-2.146-.037-3.385-1.588-4.192 1.13-.26 1.98-1.003 2.207-2.538zm-3.95 5.538c-.533 2.147-4.148.986-5.32.695l.95-3.805c1.172.293 4.929.872 4.37 3.11zm.535-5.569c-.487 1.953-3.495.96-4.47.717l.86-3.45c.975.243 4.118.696 3.61 2.733z"/></g></svg>""")
+
         with trc:
-            st.markdown(f'<div style="text-align: right; color: white; font-weight: bold; font-size: 16px;">{price:,.2f}</div>', unsafe_allow_html=True)
+            st.markdown(f'<div style="text-align: right; color: white; font-weight: bold; font-size: 16px;">${price:,.2f}</div>', unsafe_allow_html=True)
 
         tlc_2, trc_2 = st.columns((5,5),gap="small")
         with tlc_2:
-            st.markdown(f'<div style="text-align: left; color: {color}; font-weight: bold; font-size: 16px;">{arrow}</div>', unsafe_allow_html=True)
+            st.markdown(f'<div style="text-align: left; color: white; font-weight: bold; font-size: 16px;">{name}</div>', unsafe_allow_html=True)
             
         with trc_2:
-            st.markdown(f'<div style="text-align: right; color: {color}; font-weight: bold; font-size: 16px;">{abs(change_percent):.2f}%</div>', unsafe_allow_html=True)
+            st.markdown(f'<div style="text-align: right; color: {color}; font-weight: bold; font-size: 16px;">{abs(change_percent):.2f}%  {arrow}</div>', unsafe_allow_html=True)
 
     st.markdown(
     """
@@ -262,9 +278,9 @@ with t_column:
 
 with fr_column:
     
-    # DOW
-    dow_values = get_dow_ticker()
-    name, price, change_percent, sign = dow_values
+    # Ethereum
+    ethereum_values = get_ethereum_data(key)
+    name, price, change_percent, sign = ethereum_values
 
     # Define the arrow symbol based on the sign of the change
     arrow = "▲" if sign == 1 else "▼" if sign == -1 else ""
@@ -276,17 +292,17 @@ with fr_column:
 
         tlc, trc = st.columns((5,5),gap="small")
         with tlc:
-            st.markdown(f'<div style="text-align: left; color: white; font-weight: bold; font-size: 16px;">{name}</div>', unsafe_allow_html=True)
+            render_svg("""<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"><g fill="none" fill-rule="evenodd"><circle cx="16" cy="16" r="16" fill="#627EEA"/><g fill="#FFF" fill-rule="nonzero"><path fill-opacity=".602" d="M16.498 4v8.87l7.497 3.35z"/><path d="M16.498 4L9 16.22l7.498-3.35z"/><path fill-opacity=".602" d="M16.498 21.968v6.027L24 17.616z"/><path d="M16.498 27.995v-6.028L9 17.616z"/><path fill-opacity=".2" d="M16.498 20.573l7.497-4.353-7.497-3.348z"/><path fill-opacity=".602" d="M9 16.22l7.498 4.353v-7.701z"/></g></g></svg>""")
             
         with trc:
-            st.markdown(f'<div style="text-align: right; color: white; font-weight: bold; font-size: 16px;">{price:,.2f}</div>', unsafe_allow_html=True)
+            st.markdown(f'<div style="text-align: right; color: white; font-weight: bold; font-size: 16px;">${price:,.2f}</div>', unsafe_allow_html=True)
 
         tlc_2, trc_2 = st.columns((5,5),gap="small")
         with tlc_2:
-            st.markdown(f'<div style="text-align: left; color: {color}; font-weight: bold; font-size: 16px;">{arrow}</div>', unsafe_allow_html=True)
+            st.markdown(f'<div style="text-align: left; color: white; font-weight: bold; font-size: 16px;">{name}</div>', unsafe_allow_html=True)
             
         with trc_2:
-            st.markdown(f'<div style="text-align: right; color: {color}; font-weight: bold; font-size: 16px;">{abs(change_percent):.2f}%</div>', unsafe_allow_html=True)
+            st.markdown(f'<div style="text-align: right; color: {color}; font-weight: bold; font-size: 16px;">{abs(change_percent):.2f}%  {arrow}</div>', unsafe_allow_html=True)
 
     st.markdown(
     """
